16c2b951f8252687182b5f3bd5b2b7efcd6a89fc
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-11-16 19:11:54 -0300

    feat: hora da pratica

---
+++ Java - criando sua primeira aplicação/perfil/perfil/src/Perfil.java
@@ -0,0 +1,8 @@
+public class Perfil {
+    
+    public static void main(String[] args) {
+        String nome = "Marcos";
+        System.out.println("Olá " + nome + "!");
+        System.out.println("Como vai hoje?");
+    }
+}
