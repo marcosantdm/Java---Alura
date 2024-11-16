723802bd2131b75d888f18c5dc71a8d0f711483b
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-11-16 19:45:10 -0300

    feat: trabalhando com textos

--- Java - criando sua primeira aplicação/primeiroprojeto/screen match/src/App.java
+++ Java - criando sua primeira aplicação/primeiroprojeto/screen match/src/App.java
@@ -16,5 +16,26 @@ public class App {
         System.out.println("Gênero: " + genero);
         String atores = "James Cameron, Aaron Paul, David Lynch, Timothy Hough, Tony Soprano, Ryan Reynolds";
         System.out.println("Atores: " + atores);
+
+        double media = (9.8 + 3.1 + 6.4) / 3;
+        System.out.println("Média dos Críticos: " + media);
+
+        String sinopse;
+        sinopse = "A guerra entre dois grupos de policiais, um dos mais experientes e determinados do mundo, é um tema central do filme. O primeiro grupo, a polícia de San Francisco, é dominado por o ex-agente James Cameron (James Cameron), que é o líder do grupo. O segundo grupo, o Excalibur";
+        System.out.println("Sinopse: " + sinopse);
+
+        String descricao;
+        descricao = """
+                Filme: Top Gun: Maverick
+                Lançamento: 2022
+                Inclusão no Plano de Filmes: false
+                Nota: 8.5
+                Diretor: James Cameron
+                Gênero: Ação
+                Atores: James Cameron, Aaron Paul, David Lynch, Timothy Hough, Tony Soprano, Ryan Reynolds
+                Média dos Críticos: 8.0
+                """;
+
+        System.out.println(descricao);
     }
 }
