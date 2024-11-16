7a66b4dcd2d13848027fc08b3851a3e58277063c
Marcos Antonio <marcos.antoniodm039@gmail.com>
2024-11-16 19:38:22 -0300

    feat: atribuindo valores ao filme

---
+++ Java - criando sua primeira aplicação/primeiroprojeto/screen match/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int ano = 2022;
+        System.out.println("Lançamento: " + ano);
+
+        boolean inclusoNoPlano = false;
+        double nota = 8.5;
+        System.out.println("Inclusão no Plano de Filmes: " + inclusoNoPlano);
+        System.out.println("Nota: " + nota);
+        String diretor = "James Cameron";
+        System.out.println("Diretor: " + diretor);
+        String genero = "Ação";
+        System.out.println("Gênero: " + genero);
+        String atores = "James Cameron, Aaron Paul, David Lynch, Timothy Hough, Tony Soprano, Ryan Reynolds";
+        System.out.println("Atores: " + atores);
+    }
+}
